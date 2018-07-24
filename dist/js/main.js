fcf3158c29673dfe48afac84803864bcbe45ba7a
cassandradesro <cdesro10@gmail.com>
2018-07-24 17:17:24 -0400

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,7 @@
-"use strict";
+'use strict';
 
-console.log("Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.");
+console.log('Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.');
+
+var scene = document.querySelector('#scene');
+var parallaxInstance = new Parallax(scene);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
 console.log(`Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.`)
+
+
+var scene = document.querySelector('#scene');
+var parallaxInstance = new Parallax(scene);
