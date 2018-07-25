0555995726e2d05ad6ded21fcec9f8c4bdf224e6
cassandradesro <cdesro10@gmail.com>
2018-07-25 16:09:58 -0400

    commits from working with chris

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,21 @@
 console.log(`Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.`)
 
 
-var scene = document.querySelector('#scene');
-var parallaxInstance = new Parallax(scene);
+// var scene = document.querySelector('#scene');
+// var parallaxInstance = new Parallax(scene);
 
-function(){
-	
-	const ratio = 16/9;
-	const size = 95;
-	const $bookContainer = document.querySelector('.book-container')
 
-	let resizeContainer = function() {
-		let windowWidth = document.documentElement.clientWidth;
-		let windowHeight = document.documentElement.clientHeight;
-		let windowRatio = windowWidth / windowHeight;
-		if (windowRatio > ratio) {
-			console.log("window is wider than 16/9")
-			$bookContainer.style.height = size + "vh";
-			$bookContainer.style.width = size * ratio + "vh";
+var controller = new ScrollMagic.Controller();
 
-		} else {
-			console.log("window is taller than 16/9")
-			$bookContainer.style.width = size + "vw";
-			$bookContainer.style.height = size * 1/ratio + "vw";
-		}
-	}
 
-	window.addEventListener('load', resizeContainer)
-	window.addEventListener('resize', resizeContainer)
-	window.addEventListener('orientationchange', resizeContainer)
-	document.body.style.height = "1000vh";
+var part1Timeline = new TimelineMax();
 
-};
+part1Timeline.from(".part1 h1", 1, {left: "-500%"});
+// part1Timeline.from(".part1 h1", 1, {});
+// part1Timeline.to(".part1 h1", 1, {left: "500%"});
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "body", duration: "1000%"})
+	.setTween(part1Timeline)
+	.addIndicators({name: "loop"}) // add indicators (requires plugin)
+	.addTo(controller);
