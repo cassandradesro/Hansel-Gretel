daa3ff79b9be70a3dbc0045128b730346ec6288c
DannyMcMillan <mcmillandanny@gmail.com>
2018-07-27 16:09:18 -0400

    added assets and positioned them to be responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,15 +6,18 @@ console.log("Hansel & Gretel Project. Developed by Cassandra Desrosiers and Dann
 // var parallaxInstance = new Parallax(scene);
 
 
-var controller = new ScrollMagic.Controller();
+// var controller = new ScrollMagic.Controller();
 
-var part1Timeline = new TimelineMax();
 
-part1Timeline.from(".part1 h1", 1, { left: "-500%" });
-// part1Timeline.from(".part1 h1", 1, {});
-// part1Timeline.to(".part1 h1", 1, {left: "500%"});
+// var part1Timeline = new TimelineMax();
 
-// build scene
-var scene = new ScrollMagic.Scene({ triggerElement: "body", duration: "1000%" }).setTween(part1Timeline).addIndicators({ name: "loop" }) // add indicators (requires plugin)
-.addTo(controller);
+// part1Timeline.from(".part1 h1", 1, {left: "-500%"});
+// // part1Timeline.from(".part1 h1", 1, {});
+// // part1Timeline.to(".part1 h1", 1, {left: "500%"});
+
+// // build scene
+// var scene = new ScrollMagic.Scene({triggerElement: "body", duration: "1000%"})
+// 	.setTween(part1Timeline)
+// 	.addIndicators({name: "loop"}) // add indicators (requires plugin)
+// 	.addTo(controller);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,17 @@ console.log(`Hansel & Gretel Project. Developed by Cassandra Desrosiers and Dann
 // var parallaxInstance = new Parallax(scene);
 
 
-var controller = new ScrollMagic.Controller();
+// var controller = new ScrollMagic.Controller();
 
 
-var part1Timeline = new TimelineMax();
+// var part1Timeline = new TimelineMax();
 
-part1Timeline.from(".part1 h1", 1, {left: "-500%"});
-// part1Timeline.from(".part1 h1", 1, {});
-// part1Timeline.to(".part1 h1", 1, {left: "500%"});
+// part1Timeline.from(".part1 h1", 1, {left: "-500%"});
+// // part1Timeline.from(".part1 h1", 1, {});
+// // part1Timeline.to(".part1 h1", 1, {left: "500%"});
 
-// build scene
-var scene = new ScrollMagic.Scene({triggerElement: "body", duration: "1000%"})
-	.setTween(part1Timeline)
-	.addIndicators({name: "loop"}) // add indicators (requires plugin)
-	.addTo(controller);
+// // build scene
+// var scene = new ScrollMagic.Scene({triggerElement: "body", duration: "1000%"})
+// 	.setTween(part1Timeline)
+// 	.addIndicators({name: "loop"}) // add indicators (requires plugin)
+// 	.addTo(controller);
