b0d563f0edbe806bdaf2e922acb0cd0f8c0b64c0
DannyMcMillan <mcmillandanny@gmail.com>
2018-07-30 21:28:23 -0400

    added animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,22 +2,24 @@
 
 console.log("Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.");
 
-// var scene = document.querySelector('#scene');
+var scene = document.querySelector('#scene');
 // var parallaxInstance = new Parallax(scene);
 
 
-// var controller = new ScrollMagic.Controller();
+var controller = new ScrollMagic.Controller();
 
+var part1Timeline = new TimelineMax();
 
-// var part1Timeline = new TimelineMax();
+part1Timeline.from(".house", 1, { left: "-150%" });
+// part1Timeline.from(".part1 h1", 1, {});
+part1Timeline.to(".house", 1, { left: "10%" });
 
-// part1Timeline.from(".part1 h1", 1, {left: "-500%"});
-// // part1Timeline.from(".part1 h1", 1, {});
-// // part1Timeline.to(".part1 h1", 1, {left: "500%"});
+// build scene
+var scene = new ScrollMagic.Scene({ triggerElement: "body", duration: "1000%" }).setTween(part1Timeline).addIndicators({ name: "loop" }) // add indicators (requires plugin)
+.addTo(controller);
 
-// // build scene
-// var scene = new ScrollMagic.Scene({triggerElement: "body", duration: "1000%"})
-// 	.setTween(part1Timeline)
-// 	.addIndicators({name: "loop"}) // add indicators (requires plugin)
-// 	.addTo(controller);
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('cloud12', 'particles.json', function() {
+//   console.log('callback - particles.js config loaded');
+// });
 //# sourceMappingURL=main.js.map
