0ff2143df841c041dadb233b946dd84e20669e79
cassandradesro <cdesro10@gmail.com>
2018-07-31 17:31:42 -0400

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,16 +5,19 @@ console.log("Hansel & Gretel Project. Developed by Cassandra Desrosiers and Dann
 // var scene = document.querySelector('#scene');
 // var parallaxInstance = new Parallax(scene);
 
+//SCROLLMAGIC WHEN YOU'RE READY
+// var controller = new ScrollMagic.Controller();
 
-var controller = new ScrollMagic.Controller();
 
-var part1Timeline = new TimelineMax();
+// var part1Timeline = new TimelineMax();
 
-part1Timeline.from(".part1 h1", 1, { left: "-500%" });
-// part1Timeline.from(".part1 h1", 1, {});
-// part1Timeline.to(".part1 h1", 1, {left: "500%"});
+// part1Timeline.from(".part1 h1", 1, {left: "-500%"});
+// // part1Timeline.from(".part1 h1", 1, {});
+// // part1Timeline.to(".part1 h1", 1, {left: "500%"});
 
-// build scene
-var scene = new ScrollMagic.Scene({ triggerElement: "body", duration: "1000%" }).setTween(part1Timeline).addIndicators({ name: "loop" }) // add indicators (requires plugin)
-.addTo(controller);
+// // build scene
+// var scene = new ScrollMagic.Scene({triggerElement: "body", duration: "1000%"})
+// 	.setTween(part1Timeline)
+// 	.addIndicators({name: "loop"}) // add indicators (requires plugin)
+// 	.addTo(controller);
 //# sourceMappingURL=main.js.map
