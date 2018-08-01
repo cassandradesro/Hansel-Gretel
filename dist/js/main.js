715d5988d2560a42a5d19626babd9e44b40d2570
cassandradesro <cdesro10@gmail.com>
2018-08-01 14:04:13 -0400

    part 2 trees positioned, part 3 and part 4 files started

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,23 +2,45 @@
 
 console.log("Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.");
 
-var scene = document.querySelector('#scene');
-// var parallaxInstance = new Parallax(scene);
+var VIEWPORTS_TALL = 3; // how tall is the body? the taller it is, the longer it will take to scroll.
+document.body.style.minHeight = VIEWPORTS_TALL * 100 + 100 + "vh";
 
-//SCROLLMAGIC WHEN YOU'RE READY
-var controller = new ScrollMagic.Controller();
+var masterTimeline = new TimelineMax({ paused: true });
 
 var part1Timeline = new TimelineMax();
+var part2Timeline = new TimelineMax();
+var part3Timeline = new TimelineMax();
+var part4Timeline = new TimelineMax();
+var part5Timeline = new TimelineMax();
 
-part1Timeline.from(".part1 h1", 1, { left: "-500%" });
-// part1Timeline.from(".part1 h1", 1, {});
-part1Timeline.to(".part1 h1", 1, { left: "500%" });
+masterTimeline.add(part1Timeline);
+masterTimeline.add(part2Timeline);
+masterTimeline.add(part3Timeline);
+masterTimeline.add(part4Timeline);
+masterTimeline.add(part5Timeline);
 
-// // build scene
-var scene = new ScrollMagic.Scene({ triggerElement: "body", duration: "1000%" }).setTween(part1Timeline).addIndicators({ name: "loop" }) // add indicators (requires plugin)
-.addTo(controller);
+// animations for part 1
+part1Timeline.staggerFrom(".part1 .hills img", 1, { y: "200%" }, -0.3);
+part1Timeline.from(".part1 .house", 1, { rotation: "720deg", opacity: 0 });
+part1Timeline.to({}, 2, {}); //pause
+part1Timeline.to(".part1 .house", 1, { rotation: "720deg", opacity: 0 });
 
-part1Timeline.from(".house", 1, { left: "-150%" });
-// part1Timeline.from(".part1 h1", 1, {});
-part1Timeline.to(".house", 1, { left: "10%" });
+part1Timeline.staggerTo(".part1 .hills img", 1, { y: "200%" }, -0.3);
+
+// animations for part 2
+
+// animations for part 3
+
+// animations for part 4
+
+// animations for part 5
+
+
+// this is the magic part.
+// tweenmax's ticker is a super efficient "ticker" that runs at about 60 times a second on most computers,
+// but will intelligently slow down on slower computers so they don't get choppyy 
+TweenMax.ticker.addEventListener("tick", function () {
+	// measure how far down the page we are, and jump the master timeline to the matching point
+	masterTimeline.progress(window.scrollY / document.documentElement.clientHeight / VIEWPORTS_TALL);
+});
 //# sourceMappingURL=main.js.map
