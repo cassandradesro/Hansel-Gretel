abd198a913c47c55d18369fc552494520fa75319
DannyMcMillan <mcmillandanny@gmail.com>
2018-08-01 12:50:04 -0400

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-console.log("Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.");
+console.log('Hansel & Gretel Project. Developed by Cassandra Desrosiers and Danny McMillan. Designed by Carol Ehreth.');
 
 var scene = document.querySelector('#scene');
 // var parallaxInstance = new Parallax(scene);
@@ -10,9 +10,16 @@ var controller = new ScrollMagic.Controller();
 
 var part1Timeline = new TimelineMax();
 
-part1Timeline.from(".part1 h1", 1, { left: "-500%" });
-// part1Timeline.from(".part1 h1", 1, {});
-part1Timeline.to(".part1 h1", 1, { left: "500%" });
+var test = new TimelineMax();
+
+var testscene = ScrollMagic.Scene({ triggerElement: 'body', duration: "1000%" }).setTween(test).addIndicators({ name: "test" }).addTo(controller);
+
+test.from(".sun", 1, { top: "10%" });
+test.to(".sun", 1, { top: "-1000%" });
+
+// part1Timeline.from(".part1 h1", 1, {left: "-500%"});
+// // part1Timeline.from(".part1 h1", 1, {});
+// part1Timeline.to(".part1 h1", 1, {left: "500%"});
 
 // // build scene
 var scene = new ScrollMagic.Scene({ triggerElement: "body", duration: "1000%" }).setTween(part1Timeline).addIndicators({ name: "loop" }) // add indicators (requires plugin)
--- src/js/main.js
+++ src/js/main.js
@@ -10,9 +10,22 @@ var controller = new ScrollMagic.Controller();
 
 var part1Timeline = new TimelineMax();
 
-part1Timeline.from(".part1 h1", 1, {left: "-500%"});
-// part1Timeline.from(".part1 h1", 1, {});
-part1Timeline.to(".part1 h1", 1, {left: "500%"});
+var test = new TimelineMax();
+
+
+
+var testscene = ScrollMagic.Scene({triggerElement: 'body', duration: "1000%"})
+	.setTween(test)
+	.addIndicators({name: "test"})
+	.addTo(controller);
+
+
+test.from(".sun", 1, {top: "10%"});
+test.to(".sun", 1, {top: "-1000%"});
+
+// part1Timeline.from(".part1 h1", 1, {left: "-500%"});
+// // part1Timeline.from(".part1 h1", 1, {});
+// part1Timeline.to(".part1 h1", 1, {left: "500%"});
 
 // // build scene
 var scene = new ScrollMagic.Scene({triggerElement: "body", duration: "1000%"})
