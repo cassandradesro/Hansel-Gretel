ab1052e7ec152fc0299b3eb6be950e167b7dbb72
DannyMcMillan <mcmillandanny@gmail.com>
2018-08-03 16:23:04 -0400

    fixing scene 1 timeline

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,24 +19,27 @@ masterTimeline.add(part3Timeline);
 masterTimeline.add(part4Timeline);
 masterTimeline.add(part5Timeline);
 
+// tweenMax.to(target,duration, {vars} stagger ammount):
+
 // animations for part 1
-part1Timeline.staggerFrom(".part1 .hills img", 1, { y: "200%" }, -0.3); //hills stagger in
-part1Timeline.from(".part1 .house", 1, { rotation: "720deg", opacity: 0 }); // house rotates in
 
-part1Timeline.to({}, 2, {}); //pause
 
-part1Timeline.to(".part1 .house", 1, { rotation: "720deg", opacity: 0 }); // house rotates out
-part1Timeline.staggerTo(".part1 .hills img", 1, { y: "200%" }, -0.3); // hills rotate out
-part1Timeline.staggerTo(".part1 .sky img", 1, { y: "200%" }, -0.3); // sky rotates out
-part1Timeline.to(".part1", 2, { opacity: 0 }); // go away
+part1Timeline.to(".title h1, .sky .scrolltext, .title h3 ", .5, { opacity: 0 }).to(".part1 .upon", 4, { opacity: 1 }).to(".storyTextIntro", 1.5, { opacity: 1 }).to(".part1 .upon", 1.5, { opacity: 0 }).to(".storyTextIntro", 1, { opacity: 0 }).staggerFrom(".part1 .hills img", 1, { y: "200%", ease: Back.easeOut }, .6) //hills stagger in
+.to(".part1 .sun", 1, { top: "15%" }).to(".part1 .house", 1, { left: "10%", opacity: 1 }).to({}, 10, {}) //pause
+.staggerTo(".part1 .hills img", 1, { y: "200%" }) // hills rotate out
+.staggerTo(".part1 .sky img", 1, { y: "200%" }); // sky rotates out
+
 
 // animations for part 2
-part2Timeline.to(".part2", 2, { opacity: 1 }); // come on scene
-part2Timeline.staggerFrom(".part2 .right-side img", 1, { y: "200%" }, -0.3);
-part2Timeline.staggerFrom(".part2 .left-side img", 1, { y: "200%" }, -0.3);
-part2Timeline.to({}, 2, {}); //pause
-part2Timeline.staggerTo(".part2 .right-side img", 1, { y: "200%" }, -0.3);
-part2Timeline.staggerTo(".part2 .left-side img", 1, { y: "200%" }, -0.3);
+// part2Timeline.from(".part2", 2, {opacity: 0}, 10)
+// part2Timeline.staggerTo(".part2", 2, {opacity: 1})
+// part2Timeline.to(".part2", 1, {opacity: 0})// come on scene
+// part2Timeline.staggerFrom(".part2 .right-side img", 1, {y: "10000%"}, -0.3);
+// part2Timeline.staggerFrom(".part2 .left-side img", 1, {y: "10000%"}, -0.3);
+// part2Timeline.to({}, 2, {}) //pause
+// part2Timeline.staggerTo(".part2 .right-side img", 1, {y: "1000%"}, -0.3);
+// part2Timeline.staggerTo(".part2 .left-side img", 1, {y: "1000%"}, -0.3);
+
 
 // animations for part 3
 
